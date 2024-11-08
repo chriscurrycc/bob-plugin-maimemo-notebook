e3f26086d6e8e48c1a5a9adf82e7642c537d198e
chriscurrycc <hichriscurry@gmail.com>
2024-11-08 18:02:16 +0800

    Add release

--- scripts/zip.js
+++ scripts/zip.js
@@ -9,12 +9,32 @@ const outputPath = path.resolve(
   "../dist/bobplugin-maimemo-notebook.bobplugin"
 );
 
+function cleanDistFolder() {
+  const distPath = path.resolve(__dirname, "../dist");
+
+  fs.readdir(distPath, (err, files) => {
+    if (err) throw err;
+
+    files.forEach((file) => {
+      const filePath = path.join(distPath, file);
+
+      if (filePath !== outputPath) {
+        fs.rm(filePath, { recursive: true, force: true }, (err) => {
+          if (err) throw err;
+          console.log(`Deleted: ${filePath}`);
+        });
+      }
+    });
+  });
+}
+
 function zipAndRename() {
   const output = fs.createWriteStream(outputPath);
   const archive = archiver("zip", { zlib: { level: 9 } });
 
   output.on("close", () => {
     console.log(`Compress finish：${outputPath}`);
+    cleanDistFolder();
   });
 
   archive.on("error", (err) => {
@@ -22,7 +42,10 @@ function zipAndRename() {
   });
 
   archive.pipe(output);
-  archive.directory("dist/", false);
+  archive.glob("**/*", {
+    cwd: path.resolve(__dirname, "../dist"),
+    ignore: [path.basename(outputPath)],
+  });
   archive.finalize();
 }
 
